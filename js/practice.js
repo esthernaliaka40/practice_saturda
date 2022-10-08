ad8b1881977e293c184e0a100eacb7497c04a98e
naliaka <esthernaliaka40@gmail.com>
2022-10-08 13:09:36 +0300

    js updated and index.html updated too

---
+++ js/practice.js
@@ -0,0 +1,8 @@
+var daysOfWeek = ['Sunday', 'Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; 
+var maleNames = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]; 
+
+var dateInput = prompt ("Enter date of birth;");
+
+var birthDate = new Date('2022-10-08');
+
